8b4a72292817082f293dd07bc346ad60d01ead45
My Name <you@example.com>
2018-02-05 20:07:21 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>макет сайта</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	    <link rel="stylesheet" href="css/style.css"/>
+	    <script src="js/script.js"></script> 
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header"><!--начало шапки сайта-->
+				<div class="golova">
+				<div class="logo"></div>
+					<div class="site-info">	
+					<div class="telefon">
+					<div><img src="/img/mts.png"></img><span class="prefix">+375(29)</span> <span class="namber">111 11 11</span></div><div><img src="/img/velcom.png"<img><span class="prefix">+375(25)</span> <span class="namber">222 22 22</span></div><div><img src="/img/mail.png"></img><a href="#" class="mail">info@steklaris.by</a></div>
+					</div>
+					<form>
+						<input type="serch" name="p" class="ground" placeholder="поиск по сайту">
+						<input type="submit" class="button" value="">
+					</form>
+				</div>
+				 <div id="head-menu">
+							<ul class="top-navigation">
+								<li><a href="#">Home</a>
+								<ul>
+									<li><a href="home1">дом 1</a></li>
+									<li><a href="home2">дом 2</a></li>
+									<li><a href="home3">дом 3</a></li>
+									<li><a href="home4">дом 4</a></li>
+								</ul>
+								</li>
+								<li><a href="#">About us</a>
+								<ul>
+									<li><a href="home1">дом 1987</a></li>
+									<li><a href="home2">дом 1234</a></li>
+									<li><a href="home3">дом 234</a></li>
+									<li><a href="home4">дом 123</a></li>
+								</ul>
+								</li>
+								<li><a href="#">srvios</a></li>
+								<li><a href="#">solution</a></li>
+								<li><a href="#">portner</a></li>
+								<li class="last"><a href="#">Contact</a></li>
+							</ul>
+							<div class="clrb"></div>
+					</div>
+					</div>	
+			</div><!-- конец шапки сайта-->
+			<div class="all_wrap">
+				<div class="tusha">
+				<div id="content-wrapp"><!-- конетент-->
+		<div id=" top content"></div>
+		<div id="bot-cintent"><!-- центральный контент-->
+			<div class="page"><!-- страница-->
+				<h1>Ручное выдувание в формы позволяет создавать изделия, схожие одно с другим.</h1>
+				<img class="article" src="img/kartinka.png" width="350" height="479">
+				Например, лабораторные колбы.
+Стеклодув набирает на кончик стеклодувной трубки расплавленное стекло, выдувает пузырь и начинает формовать его, постоянно вращая трубочку и формует стекло в деревянных или металлических формах.
+Свободное выдувание заключается в свободном формовании изделия.
+Стеклянные предметы, выполненные путём свободного выдувания, называют также гутным стеклом (иногда называют гутенским, от немецкого hutte — гута, стеклодувная мастерская).
+Через труббку выдувается расплавленный шар. Его подправляют деревянными брусками, щипцами.<div class="albom"><div class="albom1"><a href="/img/1.jpg"><img src="/img/1.jpg"></a><a href="/img/2.jpg"><img src="/img/2.jpg"></a><a href="/img/3.jpg"><img src="/img/3.jpg"></a><a href="/img/4.jpg"><img src="/img/4.jpg"></a></div>
+<div class="albom2"><a href="/img/5.jpg"><img src="/img/5.jpg"></a><a href="/img/6.jpg"><img src="/img/6.jpg"></a><a href="/img/7.jpg"><img src="/img/7.jpg"></a><a href="/img/8.jpg"><img src="/img/8.jpg"></a></a></div></div> То, что получилось, снимают с трубки и помещают на железный стержень ("понтий" ) и продолжают обработку. При необходимости мастер раскрывает верх или раскатывают нижнюю часть, вытягивает, гнёт, режет вязкое стекло с помощью специальных инструментов.
+Нет двух совершенно одинаковых предметов, выполненных таким образом. Для них характерны тонкие стенки и прозрачность.
+
+<p align="right">Как происходит работа с печью и свободное формование, показывает ролик в конце статьи.</p>
+
+Прессовыдувание.  Будущее изделие сначала формуют в пресс-форме, а затем в горячем виде — воздухом. Изделия получаются более толстостенными, менее прозрачными. Но этот способ позволяет создать на них рельефные украшения.
+
+Итак, изделие <a href="#" class="sbllka">сформировано</a>.
+Дальше – отжиг. Так называют нагревание до 530–580°С с последующим медленным охлаждением.
+При быстром и неравномерном охлаждении после формования в стекле возникают остаточные напряжения, которые со временем приведут к тому, что изделие разрушится само собой, без видимых причин.
+<table class="t1">
+<caption>рандомных цыфр таблица</caption>
+<thead>
+<tr>
+<th>iljhu</th>
+<th>iuhgiu</th>
+<th>uyg</th>
+<th>uyguj</th>
+</tr>
+</thead>
+<tbody>
+<tr><td><a href="#">Обычное м1 закаленное</a></td>
+<td>прозрачное</td>
+<td class="pri">
+<span class="byn1">70.0 руб.</span>
+<br>
+<span class="usd">36 у.е.</span></td>
+<td class="pri">
+<span class="byn1">70.2 руб.</span>
+<br>
+<span class="usd">36 у.е.</span></td>
+<tr><td><a href="#">Обычное м2 закаленное</a></td>
+<td>прозрачное1</td>
+<td class="pri1">
+<span class="byn">123.0 руб</span>
+<br>
+<span class="u.e.">12.0 u.e.</span></td>
+<td class="pri1">
+<span class="byn">124.0 руб</span>
+<br>
+<span class="u.e.">12.0 u.e.</span></td>
+</tr>
+</tbody>
+</table>
+Отжиг позволяет уменьшить эти остаточные напряжения и сделать стекло долговечным.
+
+Как говорят сами стеклодувы, стекло – это живая субстанция. И только мастерство может сделать его послушным.
+<div class="marker"><ul>
+	<li>список</li>
+	<ul><li>подпункт</li></ul>
+	<li>список1</li>
+	<li>список2</li>
+</ul>
+</div>
+<ol>
+<li>нумерованный</li>
+<ul><li>маркерованный подпункт</li></ul>
+<li> йвцй</li>
+<li> йвц йц</li>
+</ol>
+Применение ремесла
+В прошлые годы труд стеклодувов применялся на промышленных предприятиях, например, в фармацевтике, где нужно было запаивать ампулы с лекарством.
+Теперь многие промышленные процессы полностью автоматизированы, а стеклодувы в первую очередь нужны там, где делается ставка на нестандартность.
+			<div class="first-block"></div>
+			<div class="second-block"></div>
+			</div><!-- конец страницы -->
+			<div class="right-sidebar"> <!-- правй сайдбар-->
+			<div class="news"></div>
+			<div class="right-menu"></div>
+			</div><!-- конец правй сайдбар-->
+		</div><!-- конец центральный контент-->
+		</div><!-- конец контента-->
+			<div id="left-sidebar"><!-- начало левого сайдбара-->
+			<div class="sale">
+				<h4 class="h4-sale">SALE</h4>
+				<div class="left-sale">
+					<a href="#" class="offer"><span class="list-titley">Акция: подключение розеток в области скинали бесплатно.</span><img src="/img/^A002041DE42600B7230D75432E32D837F852647F2DA5BC3785^pimgpsh_mobile_save_distr.jpg"><b>При заказе скинали с подсветкой 1 метр подсветки за 9,90 руб. В данную стоимость входит лента на выбор (от 6 до 14.4. Вт/м), кнопка включения, провода...</b></a>
+					<a href="#" class="offer1"><span class="list-titley">Акция: при заказе скинали 1 метр рейлинга или светильник 600 мм бесплатно.<img src="/img/svetilnik_releng_besplatno_smallest.jpg"><b>Подключение розеток в области скинали бесплатно. Акция действительна в случае заказа скинали в нашей компании.</b></span></a>
+					<a href="#" class="offer2"><span class="list-titley">Акция: при заказе скинали с подсветкой 1 метр подсветки за 9,90 руб.<img src="/img/akciya_skinali_podsvetka_smallest.jpg"><b>При заказе скинали с подсветкой 1 метр подсветки за 9,90 руб. В данную стоимость входит лента на выбор (от 6 до 14.4. Вт/м), кнопка включения, провода...</b></span></a>
+				</div>
+				</div>
+				<div class="article1">
+					<h4 calss="article2">ARTICLE</h4>
+					<div class="left-article">
+						<a href="#" class="article3"><span class="list-titley">Скрыть газовые трубы на кухн<img src="/img/gazovye_truby_na_kuhne_smallest.jpg"><b>Если газовые трубы на кухне портят вид, то эту проблему можно решить, закрыв их панелью скинали и сэкономив...</b></span></a>
+					</div>
+			</div>
+		</div><!-- конец левого сайдбара-->
+		</div>
+		</div>
+				<div id="footer">
+			<div class="foo-menu">
+			<ul class="bot-navigation">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About us</a></li>
+								<li><a href="#">srvios</a></li>
+								<li><a href="#">solution</a></li>
+								<li><a href="#">portner</a></li>
+								<li class="last"><a href="#">Contact</a></li>
+							</ul>
+			</div>
+			<div class="copy-left"></div>
+		</div>
+		<a href="http://hacebook.com" title="вверх" class="topbutton" onclick="jumptop();return false;">Up</a>
+		<script>a1=document.getElementById('footer');
+		a1.style.borderTop="2px solid #008dd2";</script>	
+	</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,2 @@
+//alert("какой нибудь текст");
+function jumptop() {window.scrollTo(0, 0);}	
